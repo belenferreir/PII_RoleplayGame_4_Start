cd8ca1a58392df5896fe9572d0fb78935098b611
CabreraClau <claudio.cabrera@correo.ucu.edu.uy>
2023-05-28 18:38:05 -0300

    Cambios de Gauntlet

---
+++ src/Library/items/Gem.cs
@@ -0,0 +1,19 @@
+namespace RoleplayGame.Items;
+
+public class Gem
+{
+    public int AttackPower
+    {
+        get
+        {
+            return 10;
+        }
+    }
+
+
+    public override string ToString()
+    {
+        return "Gem";
+    }
+}
+ 
