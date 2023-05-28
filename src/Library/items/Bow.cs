91d0772a17fdcc810d63a48d3ccb2da9c8670f2d
Sherfled <diego.zucca@correo.ucu.edu.uy>
2023-05-28 16:39:29 -0300

    Bow created

---
+++ src/Library/items/Bow.cs
@@ -0,0 +1,19 @@
+
+namespace RoleplayGame.Items
+{
+    public class Bow : IAttackItem
+    {
+        public int AttackPower
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Bow";
+        }
+    }
+}
